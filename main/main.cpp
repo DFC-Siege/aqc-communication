3feb136f0b7430fb515a0d5bb0d844a3cf26c9a3
Daniel da Silva <dsilvaba@pm.me>
2026-03-15 00:59:22 +0100

    WIP

--- main/main.cpp
+++ main/main.cpp
@@ -7,9 +7,7 @@
 
 #include "ble/ble_manager.hpp"
 #include "communication_handler.hpp"
-#include "i_logger.hpp"
 #include "logger.hpp"
-#include "nvs/nvs_store.hpp"
 #include "serial/serial_manager.hpp"
 #include "serial_logger.hpp"
 
@@ -17,47 +15,30 @@ extern "C" {
 void app_main(void) {
         Logging::Logger::set(std::make_unique<Logging::SerialLogger>());
 
-        auto store_result = Storage::KV::NVSStore::init("default");
-        if (store_result.failed()) {
-                Logging::logger().print_fmt("error initializing store: {}",
-                                            store_result.error());
-                return;
+        esp_err_t ret = nvs_flash_init();
+        if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+            ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+                nvs_flash_erase();
+                nvs_flash_init();
         }
 
         auto &ble = BLE::BLEManager::instance();
         ble.on_connection_changed([](bool connected) {
-                Logging::logger().print_fmt(
+                Logging::logger().println_fmt(
                     "BLE {}", connected ? "connected" : "disconnected");
         });
-        static constexpr auto BLE_NAME = "aqc";
-        ble.begin(BLE_NAME);
+        ble.begin("aqc");
 
         Serial::SerialManager serial_manager;
         serial_manager.on_receive([](std::span<const uint8_t> data) {
-                std::string str(reinterpret_cast<const char *>(data.data()),
-                                data.size());
-                Logging::logger().println("serial", str);
+                Logging::logger().println(
+                    "serial", std::string_view(
+                                  reinterpret_cast<const char *>(data.data()),
+                                  data.size()));
         });
 
         Communication::CommunicationHandler communication_handler(
             ble, serial_manager);
-
-        const std::string str = "yee";
-        const auto send_result = communication_handler.ble_transporter.send(
-            0x01,
-            std::span<const uint8_t>(
-                reinterpret_cast<const uint8_t *>(str.data()), str.size()),
-            []() { Logging::logger().println("Success!!!!!"); },
-            [](std::string_view error) {
-                    Logging::logger().println_fmt(
-                        Logging::LogLevel::Error,
-                        "on_error: error sending command: {}", error);
-            });
-        if (send_result.failed()) {
-                Logging::logger().println_fmt(
-                    Logging::LogLevel::Error,
-                    "result: error sending command: {}", send_result.error());
-        }
 
         uint32_t counter = 0;
         TickType_t last_wake_time = xTaskGetTickCount();
@@ -69,20 +50,23 @@ void app_main(void) {
                 TickType_t current_time = xTaskGetTickCount();
                 if (current_time - last_wake_time >= interval) {
                         last_wake_time = current_time;
-                        const auto str =
-                            "Count: " + std::to_string(counter++) + "\n";
-                        Logging::logger().print_fmt("sending: {}", str);
-                        serial_manager.send(std::span<const uint8_t>(
-                            reinterpret_cast<const uint8_t *>(str.data()),
-                            str.size()));
+                        const auto str = std::to_string(counter++);
+                        Logging::logger().println_fmt("count: {}", str);
 
-                        if (ble.is_connected()) {
-                                auto bytes = std::span<const uint8_t>(
-                                    reinterpret_cast<const uint8_t *>(
-                                        str.data()),
-                                    str.size());
-                                ble.send(bytes);
-                        }
+                        const auto span = std::span<const uint8_t>(
+                            reinterpret_cast<const uint8_t *>(str.data()),
+                            str.size());
+
+                        communication_handler.ble_transporter.send(
+                            0x01, span,
+                            []() {
+                                    Logging::logger().println("send complete");
+                            },
+                            [](std::string_view error) {
+                                    Logging::logger().println_fmt(
+                                        Logging::LogLevel::Error,
+                                        "send error: {}", error);
+                            });
                 }
 
                 vTaskDelay(1);
