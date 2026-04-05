346843abd33220ca1e6f18f16641a1e417ae3419
Daniel da Silva <dsilvaba@pm.me>
2026-04-05 02:38:48 +0200

    Working

--- main/main_posix.cpp
+++ main/main_posix.cpp
@@ -28,6 +28,70 @@ enum Command : transport::CommandId {
         Ping,
 };
 
+// int main(int argc, char *argv[]) {
+//         auto logger = std::make_unique<logging::ConsoleLogger>();
+//         logger->set_level(logging::LogLevel::Debug);
+//         logging::Logger::set(std::move(logger));
+//
+//         if (argc < 2) {
+//                 logging::logger().println(logging::LogLevel::Error, TAG,
+//                                           "usage: communication <device>");
+//                 return 1;
+//         }
+//
+//         const auto DEVICE = argv[1];
+//         static constexpr uint16_t MTU = 24;
+//         static constexpr uint16_t MAX_TRIES = 1;
+//
+//         serial::SerialHal serial_hal(DEVICE);
+//         transport::SerialTransporter serial_transporter(serial_hal, MTU);
+//         transport::Multiplexer multiplexer(serial_transporter);
+//
+//         using MuxChannel =
+//             transport::Multiplexer<transport::SerialTransporter>::InnerChannel;
+//         using ChunkedMuxChannel = transport::ChunkedTransporter<MuxChannel>;
+//
+//         auto &inner_channel =
+//             multiplexer.create_inner_channel(Channel::Chunked);
+//         auto chunked =
+//             std::make_unique<ChunkedMuxChannel>(inner_channel, MAX_TRIES);
+//         auto &channel =
+//             multiplexer.register_channel(Channel::Chunked,
+//             std::move(chunked));
+//
+//         transport::Dispatcher dispatcher(channel);
+//
+//         dispatcher.register_handler(
+//             Command::Ping, [](result::Result<transport::Data> result) {
+//                     if (result.failed()) {
+//                             logging::logger().println(logging::LogLevel::Error,
+//                                                       TAG, result.error());
+//                             return;
+//                     }
+//                     const auto data = std::move(result).value();
+//                     logging::logger().println(
+//                         logging::LogLevel::Info, TAG,
+//                         std::string(data.begin(), data.end()));
+//             });
+//
+//         static constexpr std::string_view msg =
+//             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//             " "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//             " "enim ad minim veniam, quis nostrud exercitation ullamco
+//             laboris " "nisi ut aliquip ex ea commodo consequat.";
+//         const auto send_result = dispatcher.send(
+//             Command::Ping, transport::Data(msg.begin(), msg.end()));
+//         if (send_result.failed()) {
+//                 logging::logger().println(logging::LogLevel::Error, TAG,
+//                                           send_result.error());
+//         }
+//
+//         while (true) {
+//                 serial_hal.loop();
+//                 std::this_thread::sleep_for(std::chrono::milliseconds(1));
+//         }
+// }
+
 int main(int argc, char *argv[]) {
         auto logger = std::make_unique<logging::ConsoleLogger>();
         logger->set_level(logging::LogLevel::Debug);
@@ -45,20 +109,10 @@ int main(int argc, char *argv[]) {
 
         serial::SerialHal serial_hal(DEVICE);
         transport::SerialTransporter serial_transporter(serial_hal, MTU);
-        transport::Multiplexer multiplexer(serial_transporter);
+        transport::ChunkedTransporter chunked_transporter(serial_transporter,
+                                                          MAX_TRIES);
 
-        using MuxChannel =
-            transport::Multiplexer<transport::SerialTransporter>::InnerChannel;
-        using ChunkedMuxChannel = transport::ChunkedTransporter<MuxChannel>;
-
-        auto &inner_channel =
-            multiplexer.create_inner_channel(Channel::Chunked);
-        auto chunked =
-            std::make_unique<ChunkedMuxChannel>(inner_channel, MAX_TRIES);
-        auto &channel =
-            multiplexer.register_channel(Channel::Chunked, std::move(chunked));
-
-        transport::Dispatcher dispatcher(channel);
+        transport::Dispatcher dispatcher(chunked_transporter);
 
         dispatcher.register_handler(
             Command::Ping, [](result::Result<transport::Data> result) {
