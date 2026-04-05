9db9337b709287c845d7626b3d2915c46bb0f50d
Daniel da Silva <dsilvaba@pm.me>
2026-04-05 16:37:02 +0200

    Fixes

--- main/main_esp32.cpp
+++ main/main_esp32.cpp
@@ -8,6 +8,7 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <hal/uart_types.h>
+#include <esp_task_wdt.h>
 
 #include "base_transporter.hpp"
 #include "chunked_transporter.hpp"
@@ -20,6 +21,7 @@
 #include "result.hpp"
 #include "serial_hal.hpp"
 #include "serial_transporter.hpp"
+#include "serializer.hpp"
 
 static constexpr auto TAG = "main";
 
@@ -29,7 +31,33 @@ enum Channel : transport::TransporterId {
 };
 
 enum Command : transport::CommandId {
-        Ping,
+        SCD,
+};
+
+struct SCDData {
+        uint16_t co2 = 0;
+        float temperature = 0.0f;
+        float humidity = 0.0f;
+        std::string error;
+
+        serializer::Data serialize() const {
+                serializer::Writer w;
+                w.write(co2);
+                w.write(temperature);
+                w.write(humidity);
+                w.write(error);
+                return std::move(w.buf);
+        }
+
+        static result::Result<SCDData> deserialize(serializer::DataView buf) {
+                serializer::Reader r{buf};
+                return result::ok(SCDData{
+                        .co2 = TRY(r.read<uint16_t>()),
+                        .temperature = TRY(r.read<float>()),
+                        .humidity = TRY(r.read<float>()),
+                        .error = TRY(r.read_string()),
+                });
+        }
 };
 
 extern "C" void app_main() {
@@ -50,17 +78,17 @@ extern "C" void app_main() {
         transport::Multiplexer multiplexer(serial_transporter);
 
         using MuxChannel =
-            transport::Multiplexer<transport::SerialTransporter>::InnerChannel;
+        transport::Multiplexer<transport::SerialTransporter>::InnerChannel;
         using ChunkedMuxChannel = transport::ChunkedTransporter<MuxChannel>;
         using DirectMuxChannel = transport::DirectTransporter<MuxChannel>;
 
         auto &inner_chunked_channel =
-            multiplexer.create_inner_channel(Channel::Chunked);
+                multiplexer.create_inner_channel(Channel::Chunked);
         auto chunked = std::make_unique<ChunkedMuxChannel>(
-            inner_chunked_channel, MAX_TRIES);
+                inner_chunked_channel, MAX_TRIES);
 
         auto &inner_direct_channel =
-            multiplexer.create_inner_channel(Channel::Direct);
+                multiplexer.create_inner_channel(Channel::Direct);
         auto direct = std::make_unique<DirectMuxChannel>(inner_direct_channel);
 
         transport::Dispatcher<transport::BaseTransporter> dispatcher;
@@ -68,58 +96,35 @@ extern "C" void app_main() {
         dispatcher.register_transporter(Channel::Direct, std::move(direct));
 
         dispatcher.register_handler(
-            Command::Ping, [](result::Result<transport::Data> result) {
-                    if (result.failed()) {
-                            logging::logger().println(logging::LogLevel::Error,
-                                                      TAG, result.error());
-                            return;
-                    }
-                    const auto data = std::move(result).value();
-                    logging::logger().println(
-                        logging::LogLevel::Info, TAG,
-                        std::string(data.begin(), data.end()));
-            });
+                Command::SCD, [](result::Result<transport::Data> result) {
+                        if (result.failed()) {
+                                logging::logger().println(logging::LogLevel::Error,
+                                                          TAG, result.error());
+                                return;
+                        }
+                        auto buffer = result.value();
+                        const auto deserialize_result = SCDData::deserialize(buffer);
+                        if (deserialize_result.failed()) {
+                                logging::logger().println(logging::LogLevel::Error,
+                                                          TAG, deserialize_result.error());
+                                return;
+                        }
+                        const auto &data = deserialize_result.value();
+                        logging::logger().println_fmt(logging::LogLevel::Info, TAG,
+                                                      "SCD co2: {} temp: {:.2f} humidity: {:.2f} error: {}",
+                                                      data.co2, data.temperature, data.humidity, data.error);
+                });
 
-        static constexpr std::string_view msg =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
-            "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut "
-            "enim ad minim veniam, quis nostrud exercitation ullamco laboris "
-            "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
-            "in reprehenderit in voluptate velit esse cillum dolore eu fugiat "
-            "nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
-            "sunt in culpa qui officia deserunt mollit anim id est laborum. "
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem "
-            "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa "
-            "quae ab illo inventore veritatis et quasi architecto beatae vitae "
-            "dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas "
-            "sit aspernatur aut odit aut fugit, sed quia consequuntur magni "
-            "dolores eos qui ratione voluptatem sequi nesciunt. Neque porro "
-            "quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, "
-            "adipisci velit, sed quia non numquam eius modi tempora incidunt "
-            "ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim "
-            "ad minima veniam, quis nostrum exercitationem ullam corporis "
-            "suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. "
-            "Quis autem vel eum iure reprehenderit qui in ea voluptate velit "
-            "esse quam nihil molestiae consequatur, vel illum qui dolorem eum ";
-        const auto send_result =
-            dispatcher.send(Channel::Chunked, Command::Ping,
-                            transport::Data(msg.begin(), msg.end()));
-        if (send_result.failed()) {
-                logging::logger().println(logging::LogLevel::Error, TAG,
-                                          send_result.error());
-        }
+        xTaskCreate([](void *arg) {
+                auto *hal = static_cast<serial::SerialHal *>(arg);
+                while (true) {
+                        hal->loop();
+                }
+        }, "serial", 8192, &serial_hal, 5, nullptr);
 
-        static constexpr std::string_view msg2 = "yee";
-        const auto send_result2 =
-            dispatcher.send(Channel::Direct, Command::Ping,
-                            transport::Data(msg2.begin(), msg2.end()));
-        if (send_result2.failed()) {
-                logging::logger().println(logging::LogLevel::Error, TAG,
-                                          send_result2.error());
-        }
+        vTaskDelete(nullptr);
 
         while (true) {
-                serial_hal.loop();
                 vTaskDelay(pdMS_TO_TICKS(1));
         }
 }
