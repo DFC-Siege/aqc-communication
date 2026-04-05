fcf0825cd6c4ad46b1ddfed20b4e79a77c2e175c
Daniel da Silva <dsilvaba@pm.me>
2026-04-05 11:53:50 +0200

    Added direct transporter

--- main/main_posix.cpp
+++ main/main_posix.cpp
@@ -9,6 +9,7 @@
 #include "base_transporter.hpp"
 #include "chunked_transporter.hpp"
 #include "console_logger.hpp"
+#include "direct_transporter.hpp"
 #include "dispatcher.hpp"
 #include "i_logger.hpp"
 #include "logger.hpp"
@@ -40,7 +41,7 @@ int main(int argc, char *argv[]) {
         }
 
         const auto DEVICE = argv[1];
-        static constexpr uint16_t MTU = 24;
+        static constexpr uint16_t MTU = 255;
         static constexpr uint16_t MAX_TRIES = 1;
 
         serial::SerialHal serial_hal(DEVICE);
@@ -50,15 +51,22 @@ int main(int argc, char *argv[]) {
         using MuxChannel =
             transport::Multiplexer<transport::SerialTransporter>::InnerChannel;
         using ChunkedMuxChannel = transport::ChunkedTransporter<MuxChannel>;
+        using DirectMuxChannel = transport::DirectTransporter<MuxChannel>;
 
-        auto &inner_channel =
+        auto &inner_chunked_channel =
             multiplexer.create_inner_channel(Channel::Chunked);
-        auto chunked =
-            std::make_unique<ChunkedMuxChannel>(inner_channel, MAX_TRIES);
-        auto &channel =
+        auto chunked = std::make_unique<ChunkedMuxChannel>(
+            inner_chunked_channel, MAX_TRIES);
+        auto &chunked_channel =
             multiplexer.register_channel(Channel::Chunked, std::move(chunked));
 
-        transport::Dispatcher dispatcher(channel);
+        auto &inner_direct_channel =
+            multiplexer.create_inner_channel(Channel::Direct);
+        auto direct = std::make_unique<DirectMuxChannel>(inner_direct_channel);
+        auto &direct_channel =
+            multiplexer.register_channel(Channel::Direct, std::move(direct));
+
+        transport::Dispatcher dispatcher(direct_channel);
 
         dispatcher.register_handler(
             Command::Ping, [](result::Result<transport::Data> result) {
@@ -90,56 +98,3 @@ int main(int argc, char *argv[]) {
                 std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
 }
-//
-// int main(int argc, char *argv[]) {
-//         auto logger = std::make_unique<logging::ConsoleLogger>();
-//         logger->set_level(logging::LogLevel::Debug);
-//         logging::Logger::set(std::move(logger));
-//
-//         if (argc < 2) {
-//                 logging::logger().println(logging::LogLevel::Error, TAG,
-//                                           "usage: communication <device>");
-//                 return 1;
-//         }
-//
-//         const auto DEVICE = argv[1];
-//         static constexpr uint16_t MTU = 24;
-//         static constexpr uint16_t MAX_TRIES = 1;
-//
-//         serial::SerialHal serial_hal(DEVICE);
-//         transport::SerialTransporter serial_transporter(serial_hal, MTU);
-//         transport::ChunkedTransporter chunked_transporter(serial_transporter,
-//                                                           MAX_TRIES);
-//
-//         transport::Dispatcher dispatcher(chunked_transporter);
-//
-//         dispatcher.register_handler(
-//             Command::Ping, [](result::Result<transport::Data> result) {
-//                     if (result.failed()) {
-//                             logging::logger().println(logging::LogLevel::Error,
-//                                                       TAG, result.error());
-//                             return;
-//                     }
-//                     const auto data = std::move(result).value();
-//                     logging::logger().println(
-//                         logging::LogLevel::Info, TAG,
-//                         std::string(data.begin(), data.end()));
-//             });
-//
-//         static constexpr std::string_view msg =
-//             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//             " "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-//             " "enim ad minim veniam, quis nostrud exercitation ullamco
-//             laboris " "nisi ut aliquip ex ea commodo consequat.";
-//         const auto send_result = dispatcher.send(
-//             Command::Ping, transport::Data(msg.begin(), msg.end()));
-//         if (send_result.failed()) {
-//                 logging::logger().println(logging::LogLevel::Error, TAG,
-//                                           send_result.error());
-//         }
-//
-//         while (true) {
-//                 serial_hal.loop();
-//                 std::this_thread::sleep_for(std::chrono::milliseconds(1));
-//         }
-// }
