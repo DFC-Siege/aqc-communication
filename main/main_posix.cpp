fe7307f57e5c6a06c4c730eef1e0e268427a8fe3
Daniel da Silva <dsilvaba@pm.me>
2026-04-05 13:37:40 +0200

    Test

--- main/main_posix.cpp
+++ main/main_posix.cpp
@@ -31,7 +31,7 @@ enum Command : transport::CommandId {
 
 int main(int argc, char *argv[]) {
         auto logger = std::make_unique<logging::ConsoleLogger>();
-        logger->set_level(logging::LogLevel::Debug);
+        logger->set_level(logging::LogLevel::Info);
         logging::Logger::set(std::move(logger));
 
         if (argc < 2) {
@@ -83,13 +83,38 @@ int main(int argc, char *argv[]) {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
             "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut "
             "enim ad minim veniam, quis nostrud exercitation ullamco laboris "
-            "nisi ut aliquip ex ea commodo consequat.";
+            "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
+            "in reprehenderit in voluptate velit esse cillum dolore eu fugiat "
+            "nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
+            "sunt in culpa qui officia deserunt mollit anim id est laborum. "
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem "
+            "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa "
+            "quae ab illo inventore veritatis et quasi architecto beatae vitae "
+            "dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas "
+            "sit aspernatur aut odit aut fugit, sed quia consequuntur magni "
+            "dolores eos qui ratione voluptatem sequi nesciunt. Neque porro "
+            "quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, "
+            "adipisci velit, sed quia non numquam eius modi tempora incidunt "
+            "ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim "
+            "ad minima veniam, quis nostrum exercitationem ullam corporis "
+            "suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. "
+            "Quis autem vel eum iure reprehenderit qui in ea voluptate velit "
+            "esse quam nihil molestiae consequatur, vel illum qui dolorem eum ";
         const auto send_result =
-            dispatcher.send(Channel::Direct, Command::Ping,
+            dispatcher.send(Channel::Chunked, Command::Ping,
                             transport::Data(msg.begin(), msg.end()));
         if (send_result.failed()) {
                 logging::logger().println(logging::LogLevel::Error, TAG,
                                           send_result.error());
+        }
+
+        static constexpr std::string_view msg2 = "yee";
+        const auto send_result2 =
+            dispatcher.send(Channel::Direct, Command::Ping,
+                            transport::Data(msg2.begin(), msg2.end()));
+        if (send_result2.failed()) {
+                logging::logger().println(logging::LogLevel::Error, TAG,
+                                          send_result2.error());
         }
 
         while (true) {
